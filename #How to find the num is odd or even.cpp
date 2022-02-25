82bf54821a1b9ab26fac0cfe423627b8cd508b24
ayushkumarsin <98596761+ayushkumarsin@users.noreply.github.com>
2022-02-25 19:56:17 +0530

    Add files via upload

---
+++ #How to find the num is odd or even.cpp
@@ -0,0 +1,24 @@
+#include<iostream>     
+using namespace std;
+
+int main(){
+	int n;
+	
+	
+	
+	cout << "Enter an Integer: ";
+	cin >> n;
+	
+	if ( n % 2 == 0)
+	  cout << n << " This num is even.";
+	  else
+	  cout << n << " This num is odd.";
+	  
+	  
+	  return 0;
+	  
+	  
+	  
+	  
+	  
+}
